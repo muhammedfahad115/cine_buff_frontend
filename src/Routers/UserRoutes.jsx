2768c4e2dfe6da848952cf0c5a4d7634d9771df6
muhammedfahad <muhammedfahadat2003@gmail.com>
2024-07-16 16:16:33 +0530

    implemented lazy loading

--- src/Routers/UserRoutes.jsx
+++ src/Routers/UserRoutes.jsx
@@ -1,42 +1,45 @@
-import { Route, Routes } from 'react-router-dom'
-import Login from '../Components/Login'
-import Signup from '../Components/Signup'
-import Home from '../Pages/Home'
-import Profile from '../Components/User/Profile'
-import User from '../Components/User/Users/User'
-import Admin from '../Components/Admin/Admin'
-import Context from '../Context/Context'
-import Auth from '../AuthGuard/Auth'
-import AddRationales from '../Components/Admin/AddRationales'
-import ShowUsers from '../Components/Admin/ShowUsers'
-import ShowRationales from '../Components/Admin/ShowRationale/ShowRationales'
-import AddMedicalBill from '../Components/Admin/AddMedicalBill/AddMedicalBill'
-import BillStatus from '../Components/Admin/BillStatus'
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Context from '../Context/Context';
+import Auth from '../AuthGuard/Auth';
+import LoadingSpinner from '../Components/LoadingSpinner/LoadingSpinner';
+
+const Login = lazy(() => import('../Components/Login'));
+const Signup = lazy(() => import('../Components/Signup'));
+const Home = lazy(() => import('../Pages/Home'));
+const Profile = lazy(() => import('../Components/User/Profile'));
+const User = lazy(() => import('../Components/User/Users/User'));
+const Admin = lazy(() => import('../Components/Admin/Admin'));
+const AddRationales = lazy(() => import('../Components/Admin/AddRationales'));
+const ShowUsers = lazy(() => import('../Components/Admin/ShowUsers'));
+const ShowRationales = lazy(() => import('../Components/Admin/ShowRationale/ShowRationales'));
+const AddMedicalBill = lazy(() => import('../Components/Admin/AddMedicalBill/AddMedicalBill'));
+const BillStatus = lazy(() => import('../Components/Admin/BillStatus'));
 
 function UserRoutes() {
   return (
-    <>
-      <Context>
+    <Context>
+      <Suspense fallback={<LoadingSpinner />}>
         <Routes>
-          <Route path='/login' element={<Login />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
           <Route path='/' element={<Auth />}>
             <Route path='/' element={<Home />}>
-              <Route path='/' element={<User />}></Route>
+              <Route path='/' element={<User />} />
               <Route path='admin' element={<Admin />}>
-                <Route index element={<AddRationales />}></Route>
-                <Route path='showusers' element={<ShowUsers />}></Route>
-                <Route path='rationales' element={<ShowRationales />}></Route>
-                <Route path='addbill' element={<AddMedicalBill />}></Route>
-                <Route path='billstatus' element={<BillStatus />}></Route>
+                <Route index element={<AddRationales />} />
+                <Route path='showusers' element={<ShowUsers />} />
+                <Route path='rationales' element={<ShowRationales />} />
+                <Route path='addbill' element={<AddMedicalBill />} />
+                <Route path='billstatus' element={<BillStatus />} />
               </Route>
-              <Route path='profile' element={<Profile />}></Route>
+              <Route path='profile' element={<Profile />} />
             </Route>
           </Route>
         </Routes>
-      </Context>
-    </>
-  )
+      </Suspense>
+    </Context>
+  );
 }
 
-export default UserRoutes
+export default UserRoutes;
